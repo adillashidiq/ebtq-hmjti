3ea06261c51145e6907a3961c9d4bd4895d452a9
adillashidiq <adill.ashidiq20@gmail.com>
2021-06-11 00:24:24 +0700

    menambahkan file add-admin

---
+++ add-admin.php
@@ -0,0 +1,77 @@
+<?php
+	include_once 'header.php';
+	if (!isset($_SESSION['user'])) {
+	  header("Location: index.php");
+	}
+?>
+	
+	<!-- Main content -->
+	<div class="main-content" id="panel">
+	  <!-- Topnav -->
+	  <nav class="navbar navbar-top navbar-expand navbar-dark bg-primary border-bottom">
+	    <div class="container-fluid">
+	      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	        <!-- Search form -->
+	  </nav>
+	  <!-- Header -->
+	  <!-- Header -->
+	  <div class="header bg-primary pb-6">
+	    <div class="container-fluid">
+	      <div class="header-body">
+	        <div class="row align-items-center py-4">
+	          <div class="col-lg-6 col-7">
+	            <h6 class="h2 text-white d-inline-block mb-0">Tugas</h6>
+	            <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
+	              <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
+	                <li class="breadcrumb-item"><a href="dashboard.php"><i class="fas fa-home"></i></a></li>
+	                <li class="breadcrumb-item"><a href="add-admin.php">Input Admin</a></li>
+	                <li class="breadcrumb-item active" aria-current="page">Tambah Admin</li>
+	              </ol>
+	            </nav>
+	          </div>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	  <!-- Page content -->
+	  <div class="container-fluid mt--6">
+	    <div class="row">
+	      <div class="col">
+	        <div class="card">
+	          <!-- Card header -->
+	          <div class="card-header border-0">
+	            <h3 class="mb-0">TAMBAH ADMIN</h3>
+	          </div>
+	          <!-- Light table -->
+	          <tr>
+	            <div class="card-body">
+	              <form action="act.php" method="post">
+				  <div class="row mb-3">
+	                  <label for="tugas" class="col-sm-2 col-form-label">NIM</label>
+	                  <div class="col-sm-10">
+	                    <input type="text" class="form-control" id="NIM" name="NIM" required="required">
+	                  </div>
+	                </div>
+	                <div class="row mb-3">
+	                  <label for="nama surah" class="col-sm-2 col-form-label">Username Admin</label>
+	                  <div class="col-sm-10">
+	                    <input type="text" class="form-control" id="username" name="username" required="required">
+	                  </div>
+	                </div>
+	                <div class="row mb-3">
+	                  <label for="tugas" class="col-sm-2 col-form-label">Password</label>
+	                  <div class="col-sm-10">
+	                    <input type="text" class="form-control" id="password" name="password" required="required">
+	                  </div>
+	                </div>
+	                <button type="submit" class="btn btn-primary" name="submit-admin">Tambah Admin</button>
+	              </form>
+	          </tr>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+<?php
+	include_once 'footer.php';
+?>
+
