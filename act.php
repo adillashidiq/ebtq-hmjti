a77334d580d2e614eaee8833cf1f100f3e002adc
adillashidiq <adill.ashidiq20@gmail.com>
2021-06-10 23:36:29 +0700

    mengganti nama database dan table

--- act.php
+++ act.php
@@ -3,14 +3,14 @@ session_start();
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$conn = mysqli_connect("localhost", "root", "", "db_login");
-$query = "SELECT * from user where username ='$username' && password='$password'";
+$conn = mysqli_connect("localhost", "root", "", "db_ebtq");
+$query = "SELECT * from admin where username ='$username' && password='$password'";
 $login = mysqli_query($conn, $query);
 $isLogin = mysqli_num_rows($login);
 
 if (isset($_POST['tblogin'])) {
     if ($isLogin == 1) {
-        $_SESSION['user'] = $username;
+        $_SESSION['admin'] = $username;
         header("Location: dashboard.php");
     } else {
         $msg = "<p class= 'alert alert-danger'> Username atau Password Anda salah</p>";
@@ -32,13 +32,13 @@ if (isset($_POST['changepwd'])) {
 }
 
 
-$conn = mysqli_connect("localhost", "root", "", "db_surahjuz30");
+$conn = mysqli_connect("localhost", "root", "", "db_ebtq");
 
 if (isset($_POST['submit-tugas'])) {
     $nama_surah = $_POST['nama_surah'];
     $tugas = $_POST['tugas'];
 
-    $query = "insert into db_surah (nama_surah, tugas) values ('$nama_surah','$tugas')";
+    $query = "insert into tugas (nama_surah, tugas) values ('$nama_surah','$tugas')";
     if (mysqli_query($conn, $query) == 'true') {
         $message = "<p class= 'alert alert-success'> Berhasil menambahkan tugas baru !</p>";
         header("Location: inputtugas.php?msg=$message");
