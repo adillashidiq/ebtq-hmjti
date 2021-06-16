c77df5fae33a4802f622c297c78bfd0c75d2d14a
adillashidiq <adill.ashidiq20@gmail.com>
2021-06-16 16:20:43 +0700

    merubah link icon home

--- mahasiswa.php
+++ mahasiswa.php
@@ -42,7 +42,7 @@ if (isset($_GET['pesan_hapus'])) {
           <h6 class="h2 text-white d-inline-block mb-0">Mahasiswa</h6>
           <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
             <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
-              <li class="breadcrumb-item"><a href="dashboard.php"><i class="fas fa-home"></i></a></li>
+              <li class="breadcrumb-item"><a href="index.php"><i class="fas fa-home"></i></a></li>
               <li class="breadcrumb-item"><a href="mahasiswa.php">Mahasiswa</a></li>
               <li class="breadcrumb-item active" aria-current="page">Mahasiswa</li>
             </ol>
