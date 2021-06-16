aae4a3b13f4f4d3ecd58a249058463bdef5daa5d
adillashidiq <adill.ashidiq20@gmail.com>
2021-06-16 12:01:44 +0700

    mengganti tampilan dashboard

--- mahasiswa.php
+++ mahasiswa.php
@@ -39,7 +39,7 @@ if (isset($_GET['pesan_hapus'])) {
     <div class="header-body">
       <div class="row align-items-center py-4">
         <div class="col-lg-6 col-7">
-          <h6 class="h2 text-white d-inline-block mb-0">Dashboard</h6>
+          <h6 class="h2 text-white d-inline-block mb-0">Mahasiswa</h6>
           <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
             <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
               <li class="breadcrumb-item"><a href="dashboard.php"><i class="fas fa-home"></i></a></li>
@@ -108,10 +108,10 @@ if (isset($_GET['pesan_hapus'])) {
           <table class="table align-items-center table-flush">
             <thead class="thead-light">
               <tr>
-                <th scope="col" class="sort" data-sort="no">No</th>
-                <th scope="col" class="sort" data-sort="nama">Username</th>
-                <th scope="col" class="sort" data-sort="nim">NIM</th>
-                <th scope="col" class="sort" data-sort="aksi">Aksi</th>
+                <th scope="col" class="sort">No</th>
+                <th scope="col" class="sort">Username</th>
+                <th scope="col" class="sort">NIM</th>
+                <th scope="col" class="sort">Aksi</th>
               </tr>
             </thead>
 
