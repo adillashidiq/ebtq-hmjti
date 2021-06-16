324b3d8f322710726d0340a19fd3f12e5d3c7c71
adillashidiq <adill.ashidiq20@gmail.com>
2021-06-16 11:00:07 +0700

    menambah file mahasiswa.php

---
+++ mahasiswa.php
@@ -0,0 +1,93 @@
+<?php
+session_start();
+if ($_SESSION['level'] == "") {
+	header("Location: login.php");
+}
+$title = 'Mahasiswa | E-BTQ HMJ TI';
+include_once 'sidenav.php';
+
+$conn = mysqli_connect("localhost", "root", "", "db_ebtq");
+$query = "select*from users where level = 'mahasiswa'";
+$result = mysqli_query($conn, $query);
+
+if (!$result) {
+	echo mysqli_error($conn);
+}
+?>
+
+<!-- Header -->
+<div class="header bg-primary pb-6">
+	<div class="container-fluid">
+		<div class="header-body">
+			<div class="row align-items-center py-4">
+				<div class="col-lg-6 col-7">
+					<h6 class="h2 text-white d-inline-block mb-0">Dashboard</h6>
+					<nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
+						<ol class="breadcrumb breadcrumb-links breadcrumb-dark">
+							<li class="breadcrumb-item"><a href="dashboard.php"><i class="fas fa-home"></i></a></li>
+							<li class="breadcrumb-item"><a href="mahasiswa.php">Mahasiswa</a></li>
+							<li class="breadcrumb-item active" aria-current="page">Mahasiswa</li>
+						</ol>
+					</nav>
+				</div>
+				<div class="col-lg-6 col-7 text-right">
+					<a href="add-mahasiswa.php" class="btn btn-sm btn-neutral">Tambah Mahasiswa</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Page content -->
+<div class="container-fluid mt--6">
+	<div class="row">
+		<div class="col">
+			<div class="card">
+				<!-- Card header -->
+				<div class="card-header border-0">
+					<h3 class="mb-0">DATA MAHASISWA</h3>
+				</div>
+				<!-- Light table -->
+				<div class="table-responsive">
+					<table class="table align-items-center table-flush">
+						<thead class="thead-light">
+							<tr>
+								<th scope="col" class="sort" data-sort="no">No</th>
+								<th scope="col" class="sort" data-sort="nama">Username</th>
+								<th scope="col" class="sort" data-sort="nim">NIM</th>
+								<th scope="col" class="sort" data-sort="aksi">Aksi</th>
+							</tr>
+						</thead>
+
+						<tbody class="list">
+							<?php $angka = 1; ?>
+							<?php foreach ($result as $data) : ?>
+								<tr>
+									<th scope="row"><?= $angka ?></th>
+									<td><?= $data['username'] ?></td>
+									<td><?= $data['nim'] ?></td>
+									<td>
+										<a href="act-mahasiswa.php?del&id=<?= $data['id'] ?>" class="btn btn-outline-danger btn-sm" onclick="return confirm('Apakah anda yakin ingin menghapus data tersebut?')">Delete</a>
+									</td>
+								</tr>
+								<?php $angka++ ?>
+							<?php endforeach ?>
+					</table>
+					<!-- end-container -->
+				</div>
+
+
+				<!-- Argon Scripts -->
+				<!-- Core -->
+				<script src="assets/vendor/jquery/dist/jquery.min.js"></script>
+				<script src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+				<script src="assets/vendor/js-cookie/js.cookie.js"></script>
+				<script src="assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+				<script src="assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+				<!-- Optional JS -->
+				<script src="assets/vendor/chart.js/dist/Chart.min.js"></script>
+				<script src="assets/vendor/chart.js/dist/Chart.extension.js"></script>
+				<!-- Argon JS -->
+				<script src="assets/js/argon.js?v=1.2.0"></script>
+				</body>
+
+				</html>
